2e9508696737534f80c3ada7a0b9e43d67b91a70
Akbarhon Avazkhodjayev <Akbarhon.tbs@gmail.com>
2019-11-20 14:15:34 -0500

    basic Flask RESTful Api with authentication

--- code/security.py
+++ code/security.py
@@ -1,32 +1,25 @@
+from user import User # Importing class User from user.py file
+from werkzeug.security import safe_str_cmp # Used for python 2.0 versions to make sure string comparsion doesn't break
+
 users = [
-	{
-		'id': 1,
-		'username': 'bob'
-		'password': 'asdas'
-	}
+	User(1, "bob", "asdf") #User class comes from user.py file 
 ]
 
+#Important notes about sets 
+#Sets do not have duplicate values(important for unique usernames and ids)
+#Elements in sets are not ordered
+#You cannot access items in a set by index 
+username_mapping = { u.username: u for u in users} #set comprehension - used to assign username(u.username) from user list(users).
+userid_mapping = {u.id: u for u in users} #set comprehension - used to assign userid(u.uid) from user list(users).
 
-username_mapping = { 'bob': {
-		'id': 1,
-		'username': 'bob',
-		'password': 'asdf'
-	}
-}
 
-userid_mapping = { 1: {
-		'id': 1,
-		'username': 'bob',
-		'password': 'asdf'
-	}	
-}
 
 
 #Retreive users using Username or UserId instead of iterating over list
 
 def authenticate(username, password):
 	user = username_mapping.get(username, None)
-	if user and user.password == password:
+	if user and safe_str_cmp(user.password, password):
 		return user
 
 def identity(payload):
