242713f6e0261ddfd116ca49318098a64e418bd5
Akbarhon Avazkhodjayev <Akbarhon.tbs@gmail.com>
2019-11-20 10:42:47 -0500

    adding security and user authentication

--- code/app.py
+++ code/app.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api
 
 
 app = Flask(__name__)
+app.secret_key = 'Kyle'
 api = Api(app)
 
 items = []
@@ -11,9 +12,11 @@ items = []
 class Item(Resource):
 	def get(self, name):
 		item = next(filter(lambda x: x['name'] == name, items), None)
-		return {'item': None}, 404
+		return {'item': None}, 200 if item else 404
 
 	def post(self, name):
+		if next(filter(lambda x: x['name'] == name, items), None):
+			return {'message' : 'An item with name'{}' already exists}'.format(name)}, 400
 		data = request.get_json() 
 		item = {'name': name, 'price': data['price']}
 		items.append(item)
